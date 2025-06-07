24567e4a6329c3a2d99c6b8dab993b067642a7ac
PrakritiChandel <123865590+PrakritiChandel@users.noreply.github.com>
2025-06-07 11:46:46 +0530

    Time: 7 ms (51.6%), Space: 13.7 MB (65.7%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // step 1:choosing any adjacent and two equal letters indx and indx+1
-        // step 2: delete those and use this string to repeat step 1
-        // step 3: return
-        // edge case if no adjacent duplicates
-
-        // find 
-        int len=s.length();
-        string ans;
-        int indx=0;
-        while(indx < s.length()){
-            if(s[indx]==s[indx+1]){
-                s.erase(indx,2);
-                indx=0;
+        string str="";
+        for(char c:s){
+            if(!str.empty() && str.back()==c){
+                str.pop_back();
             }
-          else indx++;
+            else{
+                str.push_back(c);
+            }
         }
-        return s;
+    return str;
     }
 };
