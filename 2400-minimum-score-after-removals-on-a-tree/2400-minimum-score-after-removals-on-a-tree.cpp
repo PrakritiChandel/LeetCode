f7993794b3de176cc9003652ad14fae589d2a8ad
PrakritiChandel <123865590+PrakritiChandel@users.noreply.github.com>
2025-07-24 16:35:23 +0530

    Time: 121 ms (76.98%), Space: 17.8 MB (98.41%) - LeetHub

--- 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
@@ -1,74 +1,104 @@
 class Solution {
+    vector<int>in;
+    vector<int>out;
+    bool ancestor(int x, int y)
+    {
+        if(in[x] <= in[y] && out[x] >= out[y])
+        return true;
+
+        return false;
+    }
+    void dfs(vector<int>adj[], vector<int>&nums, int node, vector<int>&vis, vector<int>&x_or, int par, int &timer)
+    {
+        vis[node] = 1;
+        x_or[node] ^= nums[node];
+        in[node] = ++timer;
+
+        for(auto &x: adj[node])
+        {
+            if(!vis[x])
+            {
+                dfs(adj, nums, x, vis, x_or, node, timer);
+                x_or[node] ^= x_or[x];
+            }
+        }
+
+        out[node] = ++timer;
+    }
 public:
     int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
         int n = nums.size();
-        vector<vector<int>> graph(n);
-        vector<unordered_set<int>> children(n);
-        vector<int> xor_val(nums);
-        vector<int> degree(n, 0);
-
-        for (const auto& e : edges) {
-            int u = e[0], v = e[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-            degree[u]++;
-            degree[v]++;
+        vector<int>adj[n];
+        in.clear();
+        in.resize(n, 0);
+        out.clear();
+        out.resize(n, 0);
+        for(auto &x: edges)
+        {
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
         }
 
-        int total = 0;
-        queue<int> q;
-        vector<bool> seen(n, false);
+        vector<int>visited(n, 0);
+        vector<int>x_or(n, 0);
+        int timer = 0;
+        dfs(adj, nums, 0, visited, x_or, -1, timer);
 
-        for (int i = 0; i < n; ++i) {
-            total ^= nums[i];
-            if (degree[i] == 1) {
-                q.push(i);
-                seen[i] = true;
+        int total_xor = x_or[0];
+       // cout<<x_or[4]<<" ";
+
+    //    for(auto &x: in)
+    //    {
+    //      cout<<x<<" ";
+    //    }
+
+    //    cout<<"\n";
+
+    //    for(auto &x: out)
+    //    {
+    //     cout<<x<<" ";
+    //    }
+
+        int answer = INT_MAX;
+
+        for(int i=1; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int a = x_or[i];
+                int b = x_or[j];
+
+            if(ancestor(i, j))
+            {
+                a ^= b;
+
+                int rem = total_xor ^ a;
+                rem ^= b;
+                int maxi = max({a, b, rem});
+                int mini = min({a, b, rem});
+                answer = min(answer, (maxi - mini)); 
+            }
+            else if(ancestor(j, i))
+            {
+                b ^= a;
+                int rem = total_xor ^ a;
+                rem ^= b;
+                int maxi = max({a, b, rem});
+                int mini = min({a, b, rem});
+                answer = min(answer, (maxi - mini)); 
+            }
+            else
+            {
+                int rem = total_xor ^ a;
+                rem ^= b;
+                int maxi = max({a, b, rem});
+                int mini = min({a, b, rem});
+                answer = min(answer, (maxi - mini)); 
+            }
             }
         }
 
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (int next : graph[cur]) {
-                if (!seen[next]) {
-                    children[next].insert(cur);
-                    children[next].insert(children[cur].begin(), children[cur].end());
-                    xor_val[next] ^= xor_val[cur];
-                }
-                degree[next]--;
-                if (degree[next] == 1 && !seen[next]) {
-                    seen[next] = true;
-                    q.push(next);
-                }
-            }
-        }
-
-        int res = INT_MAX;
-        int m = edges.size();
-        for (int i = 0; i < m - 1; ++i) {
-            for (int j = i + 1; j < m; ++j) {
-                int a = edges[i][0], b = edges[i][1];
-                if (children[a].count(b)) swap(a, b);
-
-                int c = edges[j][0], d = edges[j][1];
-                if (children[c].count(d)) swap(c, d);
-
-                vector<int> vals;
-                if (children[a].count(c)) {
-                    vals = {xor_val[c], xor_val[a] ^ xor_val[c], total ^ xor_val[a]};
-                } else if (children[c].count(a)) {
-                    vals = {xor_val[a], xor_val[c] ^ xor_val[a], total ^ xor_val[c]};
-                } else {
-                    vals = {xor_val[a], xor_val[c], total ^ xor_val[a] ^ xor_val[c]};
-                }
-
-                int max_v = *max_element(vals.begin(), vals.end());
-                int min_v = *min_element(vals.begin(), vals.end());
-                res = min(res, max_v - min_v);
-            }
-        }
-
-        return res;
+      
+        return answer;
     }
 };
