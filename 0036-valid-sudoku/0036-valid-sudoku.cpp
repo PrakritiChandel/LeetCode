d9e99ee430a7d0f2cb94a05552d36196f9b6c098
PrakritiChandel <123865590+PrakritiChandel@users.noreply.github.com>
2025-08-31 19:49:20 +0530

    Time: 4 ms (52.55%), Space: 24.3 MB (44.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,49 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        bool rows[9][9] = {false};
-        bool cols[9][9] = {false};
-        bool boxes[9][9] = {false};
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    int num = board[i][j] - '1';
-                    int boxIndex = (i / 3) * 3 + (j / 3);
-                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
-                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
-                }
+    bool validSub(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
+        unordered_set<char> st;
+        for(int row = sr; row<=er; row++) {
+            for(int col = sc; col <= ec; col++) {
+                char ch = board[row][col];
+                if(ch == '.') continue;
+                if(st.count(ch)) return false;
+                st.insert(ch);
             }
         }
+        return true;
+    }
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        //row
+        for(int i=0;i<9;i++){
+            unordered_set<int> st;
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(st.count(board[i][j])) return false;
+                st.insert(board[i][j]);
+            }
+        }
+
+        // col
+        for(int i=0;i<9;i++){
+            unordered_set<int> st;
+            for(int j=0;j<9;j++){
+                if(board[j][i]=='.') continue;
+                if(st.count(board[j][i])) return false;
+                st.insert(board[j][i]);
+            }
+        }
+
+        //validate ech 3*3 box
+        for(int sr = 0; sr < 9; sr+=3) {
+            int er = sr+2;
+            for(int sc = 0; sc < 9; sc+=3) {
+                int ec = sc+2;
+                if(!validSub(board, sr, er, sc, ec))
+                    return false;
+            }
+        }
+        
         return true;
     }
 };
