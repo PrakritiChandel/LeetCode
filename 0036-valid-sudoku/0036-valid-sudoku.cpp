e9c98695b13bc7fc2eaf88ea5f6912da885a2fa0
PrakritiChandel <123865590+PrakritiChandel@users.noreply.github.com>
2025-08-31 19:51:50 +0530

    Time: 12 ms (11.39%), Space: 25.9 MB (12.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,46 +1,66 @@
 class Solution {
 public:
-    bool validSub(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
-        unordered_set<char> st;
-        for(int row = sr; row<=er; row++) {
-            for(int col = sc; col <= ec; col++) {
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.count(ch)) return false;
-                st.insert(ch);
-            }
-        }
-        return true;
-    }
 
-    bool isValidSudoku(vector<vector<char>>& board) {
-        //row
-        for(int i=0;i<9;i++){
-            unordered_set<int> st;
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.') continue;
-                if(st.count(board[i][j])) return false;
-                st.insert(board[i][j]);
-            }
-        }
+    // bool validSub(vector<vector<char>>& board, int sr, int er, int sc, int ec) {
+    //     unordered_set<char> st;
+    //     for(int row = sr; row<=er; row++) {
+    //         for(int col = sc; col <= ec; col++) {
+    //             char ch = board[row][col];
+    //             if(ch == '.') continue;
+    //             if(st.count(ch)) return false;
+    //             st.insert(ch);
+    //         }
+    //     }
+    //     return true;
+    // }
 
-        // col
-        for(int i=0;i<9;i++){
-            unordered_set<int> st;
-            for(int j=0;j<9;j++){
-                if(board[j][i]=='.') continue;
-                if(st.count(board[j][i])) return false;
-                st.insert(board[j][i]);
-            }
-        }
+    // bool isValidSudoku(vector<vector<char>>& board) {
+    //     //row
+    //     for(int i=0;i<9;i++){
+    //         unordered_set<int> st;
+    //         for(int j=0;j<9;j++){
+    //             if(board[i][j]=='.') continue;
+    //             if(st.count(board[i][j])) return false;
+    //             st.insert(board[i][j]);
+    //         }
+    //     }
 
-        //validate ech 3*3 box
-        for(int sr = 0; sr < 9; sr+=3) {
-            int er = sr+2;
-            for(int sc = 0; sc < 9; sc+=3) {
-                int ec = sc+2;
-                if(!validSub(board, sr, er, sc, ec))
-                    return false;
+    //     // col
+    //     for(int i=0;i<9;i++){
+    //         unordered_set<int> st;
+    //         for(int j=0;j<9;j++){
+    //             if(board[j][i]=='.') continue;
+    //             if(st.count(board[j][i])) return false;
+    //             st.insert(board[j][i]);
+    //         }
+    //     }
+
+    //     //validate ech 3*3 box
+    //     for(int sr = 0; sr < 9; sr+=3) {
+    //         int er = sr+2;
+    //         for(int sc = 0; sc < 9; sc+=3) {
+    //             int ec = sc+2;
+    //             if(!validSub(board, sr, er, sc, ec))
+    //                 return false;
+    //         }
+    //     }
+        
+    //     return true;
+
+        bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        
+        for(int i = 0; i<9; i++) {
+            for(int j = 0; j<9; j++) {
+                if(board[i][j] == '.') continue;
+                
+                string row = string(1, board[i][j]) + "_row_" + to_string(i);
+                string col = string(1, board[i][j]) + "_col_" + to_string(j);
+                string box = string(1, board[i][j]) + "_box_" + to_string(i/3) + "_" + to_string(j/3);
+                if(st.count(row) || st.count(col) || st.count(box)) return false;
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
             }
         }
         
